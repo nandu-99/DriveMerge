7ede5b6b2ea06f67f3350fef2066281d29374c9f
Pottabathini Vivekananda <vivekananda.99666@gmail.com>
2025-11-01 08:40:02 +0530

    Implemented Google OAuth login (#7)

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,11 +4,13 @@ const {
   register,
   login,
   getProfile,
+  googleLogin,
 } = require("../controllers/authController");
 const { authMiddleware } = require("../middlewares/authMiddleware");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", authMiddleware, getProfile);
+router.post("/google-login", googleLogin);
 
 module.exports = router;
